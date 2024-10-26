e19cb3b159ecf86bf9df11a20322e317db1ab706
wines1 <74620550+wines1@users.noreply.github.com>
2024-10-26 14:32:44 -0400

    Create config.py

---
+++ chipsff/config.py
@@ -0,0 +1,21 @@
+from typing import List
+from typing import Literal
+from typing import Dict
+from pydantic_settings import BaseSettings
+from typing import Optional
+
+
+class CHIPSFFConfig(BaseSettings):
+    jid: Optional[str] = None
+    jid_list: Optional[List[str]] = None
+    calculator_type: str = "alignn_ff"
+    calculator_types: Optional[List[str]] = ["alignn_ff"]
+    chemical_potentials_file: str = "chemical_potentials.json"
+    film_id: Optional[List[str]] = None
+    film_index: str = "0_0_1"
+    substrate_id: Optional[List[str]] = None
+    substrate_index: str = "0_0_1"
+    relaxation_settings: Optional[dict] = None
+    phonon_settings: Optional[dict] = None
+    properties_to_calculate: Optional[List[str]] = None
+    surface_indices_list: Optional[List[List[int]]] = None
