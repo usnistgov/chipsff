43fe46d00be88c89f35ac137c707a3f79305ddb2
wines1 <74620550+wines1@users.noreply.github.com>
2024-11-15 10:15:53 -0500

    Delete tests/test_config.py

--- tests/test_config.py
+++
@@ -1,7 +0,0 @@
-from chipsff.config import CHIPSFFConfig
-
-def test_default_config():
-    config = CHIPSFFConfig()
-    assert config.calculator_type == 'alignn_ff'
-    assert isinstance(config.properties_to_calculate, list)
-
